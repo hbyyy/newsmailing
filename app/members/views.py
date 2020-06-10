af1a011aa45a18895bd3b05b38241500c3d0248a
hbyyy <qjaduddl94@gmail.com>
2020-06-10 23:36:33 +0900

    feat: base 템플릿 추가

--- app/members/views.py
+++ app/members/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import FormView
+
+from members.forms import SubscribeForm
+
+
+def testview(request):
+    return render(request, 'index.html')
+
+
+class Index(FormView):
+    template_name = 'index.html'
+    form_class = SubscribeForm
+    success_url = '/'
+
+    def form_valid(self, form):
+        return super().form_valid()
