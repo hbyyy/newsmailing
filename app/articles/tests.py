1543fac95889a847eeaa0591c922a24d5c746817
hbyyy <qjaduddl94@gmail.com>
2020-06-11 21:16:22 +0900

    feat: 인증 기간이 끝난 user 처리 test code 작겅

--- app/articles/tests.py
+++ app/articles/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+import pytest
+
 
 # Create your tests here.
+
+
+@pytest.mark.django_db
+class TestArticlesModel:
+    pytestmark = pytest.mark.django_db
+
+    def test_article(self):
+        pass
