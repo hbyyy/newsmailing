ae34e9e4595578b3f0286aa6ba66760c5e2cec28
hbyyy <qjaduddl94@gmail.com>
2020-06-14 15:19:38 +0900

    feat: email-auth test code 작성

--- app/members/tests/test_email_auth.py
+++ app/members/tests/test_email_auth.py
@@ -8,10 +8,12 @@ from config.settings import EMAIL_HOST_USER
 @pytest.mark.django_db
 def test_email_auth(django_user_model, client):
     faker = Faker()
+    # 테스트를 위한 유저 생성
     user = django_user_model.objects.create_user(email=faker.email())
 
     assert user.is_active is False
 
+    # 인증메일 보내는 기능 테스트
     mail.send_mail('test에서 보낸 메일입니다',
                    'test에서 보낸 메일입니다',
                    EMAIL_HOST_USER,
@@ -24,3 +26,9 @@ def test_email_auth(django_user_model, client):
     assert mail.outbox[0].from_email == EMAIL_HOST_USER
     assert mail.outbox[0].to == ['qjaduddl94@gmail.com']
 
+    # 메일 인증 과정 -> user의 is_active가 True로 설정되어야 함
+
+    client.get(user.get_absolute_url())
+
+    user = django_user_model.objects.latest()
+    assert user.is_active is True
--- app/members/urls.py
+++ app/members/urls.py
@@ -4,5 +4,5 @@ from members.views import email_auth
 
 app_name = 'members'
 urlpatterns = [
-    path('auth/<str:token>/', email_auth, name='email-auth')
+    path('auth/<str:token>/', email_auth, name='email-auth'),
 ]
