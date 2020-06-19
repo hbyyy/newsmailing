e46c5829db819a84b0e90e54b751ece39d2f97fa
BeomYeongHeo <37058561+hbyyy@users.noreply.github.com>
2020-06-20 00:27:41 +0900

    Feature/scraper (#12)
    * feat: scrapy project 생성, 기사를 수집하는 spider 구현
    
    * feat: scrapy-djangoitem 패키지 추가, spider 에 코드 추가
    
    * feat: scrapy-djangoitem 이용해서 크롤링한 데이터 db에 저장
    
    * feaet: 크롤링 코드 완성

--- app/articles/admin.py
+++ app/articles/admin.py
@@ -1,3 +1,11 @@
-from django.contrib import admin
+
 
 # Register your models here.
+from django.contrib import admin
+
+from articles.models import Company
+
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    pass
