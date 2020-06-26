43ddc3c88635d0fdc5ba1b4538491ad45c5ed6bf
hbyyy <qjaduddl94@gmail.com>
2020-06-26 19:17:52 +0900

    feat: test view 작성

--- app/articles/views.py
+++ app/articles/views.py
@@ -1,9 +1,12 @@
 # Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from django.views.generic import ListView
 
 from articles.models import Article
 
 
+@method_decorator(login_required, name='dispatch')
 class TestArticleView(ListView):
     model = Article
     template_name = 'test/testview.html'
